c88a553c48a86703af531c414445eec293262599
khaleedahmmed <khaleed.ahmmed@gmail.com>
2023-08-23 20:25:19 +0200

    some refactor for maping function

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -9,63 +9,61 @@ class UserController extends Controller
 {
     public function index(Request $request)
     {
-        // Get a list of JSON files in the data directory
-        $jsonFiles = Storage::disk('local')->files('json_data');
+        try {
+            // Get a list of JSON files in the data directory
+            $jsonFiles = Storage::disk('local')->files('json_data');
 
-        // The combined data
-        $combinedData = [];
+            // The combined data
+            $combinedData = [];
 
-        // Loop through the JSON files
-        foreach ($jsonFiles as $jsonFile) {
-            // Extract the filename without the extension as the "source"
-            $source = pathinfo($jsonFile, PATHINFO_FILENAME);
+            foreach ($jsonFiles as $jsonFile) {
+                $source = pathinfo($jsonFile, PATHINFO_FILENAME);
 
-            // Load data from the JSON file with standard convert
-            $jsonData = $this->standardizeJson($source,json_decode(Storage::disk('local')->get($jsonFile), true));
+                $jsonData = $this->standardizeJson($source, json_decode(Storage::disk('local')->get($jsonFile), true));
 
-            // Add the "source" to each user's data
-            foreach ($jsonData as $user) {
-                $user['source'] = $source;
-                $combinedData[] = $user;
+                foreach ($jsonData as $user) {
+                    $user['source'] = $source;
+                    $combinedData[] = $user;
+                }
             }
-        }
 
-        // Apply filters
-        if ($request->has('provider')) {
-            $combinedData = array_filter($combinedData, function ($user) use ($request) {
-                $provider = $request->input('provider');
-                return $user['source'] === $provider;
-            });
-        }
+            // Apply filters
+            if ($request->has('provider')) {
+                $combinedData = array_filter($combinedData, function ($user) use ($request) {
+                    $provider = $request->input('provider');
+                    return $user['source'] === $provider;
+                });
+            }
 
-        if ($request->has('statusCode')) {
-            $statusCode = $this->getStatusCode($request->input('statusCode'));
-            $combinedData = array_filter($combinedData, function ($user) use ($statusCode) {
-                return $user['status'] === $statusCode;
-            });
-        }
+            if ($request->has('statusCode')) {
+                $statusCode = $this->getStatusCode($request->input('statusCode'));
+                $combinedData = array_filter($combinedData, function ($user) use ($statusCode) {
+                    return $user['status'] === $statusCode;
+                });
+            }
 
-        if ($request->has('balanceMin')) {
-            $combinedData = array_filter($combinedData, function ($user) use ($request) {
-                return $user['balance'] >= $request->input('balanceMin');
-            });
-        }
+            if ($request->has('balanceMin')) {
+                $combinedData = array_filter($combinedData, function ($user) use ($request) {
+                    return $user['balance'] >= $request->input('balanceMin');
+                });
+            }
 
-        if ($request->has('balanceMax')) {
-            $combinedData = array_filter($combinedData, function ($user) use ($request) {
-                return $user['balance'] <= $request->input('balanceMax');
-            });
-        }
+            if ($request->has('balanceMax')) {
+                $combinedData = array_filter($combinedData, function ($user) use ($request) {
+                    return $user['balance'] <= $request->input('balanceMax');
+                });
+            }
 
-        if ($request->has('currency')) {
-            $currency = $request->input('currency');
-            $combinedData = array_filter($combinedData, function ($user) use ($currency) {
-                return strcasecmp($user['currency'], $currency) === 0;
-            });
+            if ($request->has('currency')) {
+                $currency = $request->input('currency');
+                $combinedData = array_filter($combinedData, function ($user) use ($currency) {
+                    return strcasecmp($user['currency'], $currency) === 0;
+                });
+            }
+            return response()->json(['data' => array_values($combinedData)], 200);
+        } catch (\Exception $e) {
+            return response()->json(['error' => 'An error occurred'], 500);
         }
-
-        // Return the filtered data as JSON
-        return response()->json(array_values($combinedData));
     }
 
     private function getStatusCode($status)
@@ -89,20 +87,24 @@ class UserController extends Controller
         // Initialize an array to store the standardized data.
         $standardizedData = [];
 
-        foreach ($data as $item) {
+        // Define a mapping function to transform each item
+        $mapFunction = function ($item) use ($mapping) {
             $standardizedItem = [];
 
-            foreach ($mapping as $sourceKey => $destinationKey) {
-                if ($sourceKey === 'status' && isset($mapping['status_mapping'][$item['status']])) {
-                    $standardizedItem[$destinationKey] = $mapping['status_mapping'][$item['status']];
-                } elseif (isset($item[$sourceKey])) {
-                    $standardizedItem[$destinationKey] = $item[$sourceKey];
+            // Map keys based on the configuration
+            array_walk($item, function ($value, $sourceKey) use ($mapping, &$standardizedItem) {
+                if (isset($mapping[$sourceKey])) {
+                    $standardizedItem[$mapping[$sourceKey]] = $value;
                 }
-            }
+            });
 
-            $standardizedData[] = $standardizedItem;
-        }
+            return $standardizedItem;
+        };
+
+        // Use array_map to apply the mapping function to each item in $data
+        $standardizedData = array_map($mapFunction, $data);
 
         return $standardizedData;
     }
+
 }
